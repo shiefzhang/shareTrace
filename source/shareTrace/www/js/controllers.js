6011412a368b6222c84a4b38ec56ee67efbfe482
shiefzhang <zhangxuefeng@batonsoft.com>
2016-03-06 11:22:50 +0800

    add ngCordova

--- source/shareTrace/www/js/controllers.js
+++ source/shareTrace/www/js/controllers.js
@@ -1,49 +1,85 @@
-angular.module('starter.controllers', ['starter.services'])
+angular.module('shareTrace.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-  var longitude = 113.738487;
-  var latitude = 34.361282;
-  $scope.mapOptions = {
-    center: {
-      longitude:longitude,
-      latitude:latitude
-    },
-    zoom:15,
-    city:'Xinzheng',
-    markers:[{
-      longitude:longitude,
-      latitude:latitude,
-      icon:'http://img.coolwp.com/wp-content/uploads/2015/04/48-map-marker.png',
-      width:48,
-      height:48,
-      title:'在哪儿',
-      content:'新郑市梨河镇'
-    }]
-  };
+  .controller('DashCtrl', function ($scope, $cordovaGeolocation) {
+    var longitude = 113.738487;
+    var latitude = 34.361282;
+    $scope.mapOptions = {
+      center: {
+        longitude: longitude,
+        latitude: latitude
+      },
+      zoom: 15,
+      city: 'Xinzheng',
+      markers: [{
+        longitude: longitude,
+        latitude: latitude,
+        icon: 'http://img.coolwp.com/wp-content/uploads/2015/04/48-map-marker.png',
+        width: 48,
+        height: 48,
+        title: '在哪儿',
+        content: '新郑市梨河镇'
+      }]
+    };
 
-})
+    var posOptions = {timeout: 10000, enableHighAccuracy: false};
+    $cordovaGeolocation
+      .getCurrentPosition(posOptions)
+      .then(function (position) {
+        var lat = position.coords.latitude
+        var long = position.coords.longitude
+      }, function (err) {
+        // error
+      });
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    var watchOptions = {
+      timeout: 3000,
+      enableHighAccuracy: false // may cause errors if true
+    };
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+    var watch = $cordovaGeolocation.watchPosition(watchOptions);
+    watch.then(
+      null,
+      function (err) {
+        // error
+      },
+      function (position) {
+        var lat = position.coords.latitude
+        var long = position.coords.longitude
+      });
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+
+    watch.clearWatch();
+    // OR
+    $cordovaGeolocation.clearWatch(watch)
+      .then(function (result) {
+        // success
+      }, function (error) {
+        // error
+      });
+  })
+
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  });
